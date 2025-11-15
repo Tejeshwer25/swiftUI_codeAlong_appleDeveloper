090fab4dec1574e09339b1cf64e478d7491eb9c2
Tejeshwer Singh <tejeshwersingh2504@gmail.com>
2025-11-16 01:32:33 +0530

    Initial commit and project setup

---
+++ GratefulMoments/GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Tejeshwer Singh on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
